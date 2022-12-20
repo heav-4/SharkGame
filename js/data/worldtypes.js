300a3ad99f31f0ac08c140237eb9648dc4cff9e2
spencers145 <smartypantsspencer@gmail.com>
2022-12-20 07:10:43 -0500

    isWorldBeaten
    Feels weird to not have a specific function for it.

--- js/data/worldtypes.js
+++ js/data/worldtypes.js
@@ -206,7 +206,7 @@ SharkGame.WorldTypes = {
             get longDesc() {
                 return (
                     "The water here is dank and tinted green by " +
-                    (gateway.completedWorlds.indexOf("abandoned") > -1
+                    (gateway.isWorldBeaten("abandoned")
                         ? sharktext.getResourceName("tar", undefined, undefined, sharkcolor.getElementColor("pane")) + "."
                         : "an unrecognizable substance.") +
                     " Husks of machinery litter the ocean floor."
@@ -217,7 +217,7 @@ SharkGame.WorldTypes = {
             get tip() {
                 return (
                     "This ocean is polluted with " +
-                    (gateway.completedWorlds.indexOf("abandoned") > -1
+                    (gateway.isWorldBeaten("abandoned")
                         ? sharktext.getResourceName("tar", undefined, undefined, sharkcolor.getElementColor("pane"))
                         : "an unrecognizable substance" + ". It is only harmful when machines produce it.")
                 );
